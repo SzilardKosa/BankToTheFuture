3c39e5677574767dfc3f707014eeeec9ab355fb3
Ritter Aron <raron997@gmail.com>
2021-03-05 00:54:26 +0100

    Shop Items done perfectly

--- src/UserInterface/ShopUI.gd
+++ src/UserInterface/ShopUI.gd
@@ -1,45 +1,23 @@
 extends Control
-#var ShopItem = load("res://src/Class/ShopItem.gd")
-var Items := []
-var wealth = 0
-var price = 0
-# Called when the node enters the scene tree for the first time.
+signal bought
+
+var shop_item_ui = []
+
 func _ready():
-	print("ALMA")
-	Add_Item_Name("Egg",150)
-	Add_Item_Name("Bread",200)
-	draw_Item_List()
-	$CanvasLayer2/PriceSum.text = str(price);
+	for _i in self.get_children():
+		if _i is Control and "ShopItemUI" in _i.name  :
+			shop_item_ui.append(_i)
+	pass
+
+func _on_Button_pressed():
+	emit_signal("bought")
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func draw_Item_List():
-	$CanvasLayer2/ItemList.clear()
-	draw_Label()
-	for item in Items:
-		$CanvasLayer2/ItemList.add_item(item.getName() + " - " + str(item.getValue()))
-
-func draw_Label():
-	$CanvasLayer2/PriceSum.text = str(price);
-	
-func _on_Accept_pressed():
-	$CanvasLayer2/ItemList.add_item("Added",null,true)
-	pass # Replace with function body.
-
-func Add_Item(item):
-	Items.append(item)
-	pass
-func Add_Item_Name(name, value):	
-	Items.append(ShopItem.new(name, value, true))
-	pass
-func _on_ItemList_multi_selected(index, selected):
-	print(index)
-	print(selected)
-	if Items.size() > index:
-		price += Items[index].getValue()
-		draw_Label()
+func _on_ShopItemUI_changed():
+	$Label.text = str($ShopItemUI2.get_value() + $ShopItemUI.get_value() + $ShopItemUI3.get_value())	
+	var value = 0
+	for siu in shop_item_ui:
+		value += siu.get_value()
+	$Label.text = str(value);
 	pass # Replace with function body.

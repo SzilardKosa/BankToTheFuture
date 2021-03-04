e0f300437576732fe5e79da40d4b6e03e983178e
Ritter Aron <raron997@gmail.com>
2021-03-04 21:45:13 +0100

    Shop 2.0

--- src/UserInterface/ShopUI.gd
+++ src/UserInterface/ShopUI.gd
@@ -1,11 +1,15 @@
 extends Control
 #var ShopItem = load("res://src/Class/ShopItem.gd")
 var Items := []
+var wealth = 0
+var price = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("ALMA")
 	Add_Item_Name("Egg",150)
+	Add_Item_Name("Bread",200)
 	draw_Item_List()
+	$CanvasLayer2/PriceSum.text = str(price);
 	pass # Replace with function body.
 
 
@@ -15,9 +19,13 @@ func _ready():
 
 func draw_Item_List():
 	$CanvasLayer2/ItemList.clear()
+	draw_Label()
 	for item in Items:
-		$CanvasLayer2/ItemList.add_item(item.getName())
+		$CanvasLayer2/ItemList.add_item(item.getName() + " - " + str(item.getValue()))
 
+func draw_Label():
+	$CanvasLayer2/PriceSum.text = str(price);
+	
 func _on_Accept_pressed():
 	$CanvasLayer2/ItemList.add_item("Added",null,true)
 	pass # Replace with function body.
@@ -29,6 +37,9 @@ func Add_Item_Name(name, value):
 	Items.append(ShopItem.new(name, value, true))
 	pass
 func _on_ItemList_multi_selected(index, selected):
+	print(index)
+	print(selected)
 	if Items.size() > index:
-		$CanvasLayer2/ItemList.add_item(str(Items[index].getValue()),null,true)
+		price += Items[index].getValue()
+		draw_Label()
 	pass # Replace with function body.

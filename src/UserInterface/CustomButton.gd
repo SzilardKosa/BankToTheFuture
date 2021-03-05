af8e60b4da4016cb4e4bec8af62bf640ac45a0f7
Varjas István Péter <istvan.varjas@generalmechatronics.com>
2021-03-05 15:54:19 +0100

    custom buttons

---
+++ src/UserInterface/CustomButton.gd
@@ -0,0 +1,33 @@
+tool
+extends TextureButton
+
+export var texture: Texture setget _set_texture
+export var label: String setget _set_label
+export var scene_to_load: PackedScene
+
+func _ready():
+	_set_texture(texture)
+	_set_label(label)
+
+func _set_texture(value):
+	texture = value
+	if !is_inside_tree():
+		return
+	get_node("Texture").texture = texture
+
+func _set_label(value):
+	label = value
+	if !is_inside_tree():
+		return
+	get_node("Label").text = label
+
+
+
+func _on_button_down():
+	get_node("Anim").play("Pressed")
+	pass # Replace with function body.
+
+
+func _on_button_up():
+	get_node("Anim").play("Unpressed")
+	pass # Replace with function body.

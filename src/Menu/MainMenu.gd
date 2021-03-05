80b88d402feaabf71e9bd5351b05062d2d0d3232
Szilárd Kósa <kszilard777@gmail.com>
2021-03-05 18:01:28 +0100

    main screen added

--- src/Menu/MainMenu.gd
+++ src/Menu/MainMenu.gd
@@ -2,11 +2,11 @@ extends Control
 
 var scene_path_to_load
 
+onready var buttons = get_node("MarginContainer/VBoxContainer/MarginContainer/Buttons")
+
 func _ready():
-	pass
-#	$MarginContainer/HBoxContainer/VBoxContainer/Buttons/StartGameButton.grab_focus()
-#	for button in $MarginContainer/HBoxContainer/VBoxContainer/Buttons.get_children():
-#		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+	for button in buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
 func _on_Button_pressed(scene_to_load):
 	$FadeIn.show()
@@ -16,4 +16,4 @@ func _on_Button_pressed(scene_to_load):
 
 func _on_FadeIn_fade_finished():
 	$FadeIn.hide()
-	get_tree().change_scene(scene_path_to_load)
+	get_tree().change_scene_to(scene_path_to_load)
---
+++ src/UserInterface/CustomIconButton.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+
+export var scene_to_load: PackedScene
+
+func _on_button_down():
+	get_node("Anim").play("Pressed")
+
+
+func _on_button_up():
+	get_node("Anim").play("Unpressed")
